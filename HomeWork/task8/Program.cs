ec9982175772cda7b52b484e12857c3a082d98f1
Иванов Алексей <89289075559@mai.ru>
2022-09-03 10:56:24 +0300

    task8

---
+++ HomeWork/task8/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите число");
+int N = Convert.ToInt32(Console.ReadLine());
+int currentValue = 2;
+while(currentValue <= N)
+{
+    Console.Write(currentValue + " ");
+    currentValue += 2;
+}
