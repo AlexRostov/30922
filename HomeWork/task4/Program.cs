410ba787969ee5a6d26a3f97aec1bbd64a42bfe7
Иванов Алексей <89289075559@mai.ru>
2022-09-03 10:27:01 +0300

    task4

---
+++ HomeWork/task4/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите первое число: ");
+            int m1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите второе число: ");
+            int m2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите третье число: ");
+            int m3 = Convert.ToInt32(Console.ReadLine());
+            if (m1 > m2)
+                if (m1 >m3)
+            {
+                Console.WriteLine(m1);
+            }
+            if (m2 > m1)
+                if (m2 >m3)
+            {
+                Console.WriteLine(m2);
+            }
+            else
+            {
+                Console.WriteLine(m3);
+            }
